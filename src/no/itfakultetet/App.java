7fbdc6187af14ba44632f02dbb499824bfe10e12
Terje Berg-Hansen <terje@itfakultetet.no>
2024-01-11 00:52:50 +0100

    fixed uri

--- src/no/itfakultetet/App.java
+++ src/no/itfakultetet/App.java
@@ -8,13 +8,16 @@ public class App {
     public static final String WORKER_ADDRESS_1 = "http://localhost:8081/task";
     public static final String WORKER_ADDRESS_2 = "http://localhost:8082/task";
 
+    public static final String WORKER_ADDRESS_3 = "http://localhost:8083/task";
+
     public static void main(String[] args) {
 
         Aggregator aggregator = new Aggregator();
         String task1 = "10,200";
         String task2 = "123456789,10000000000000,70000000000023452";
-        List<String> results = aggregator.sendTaskToWorkers(Arrays.asList(WORKER_ADDRESS_1,WORKER_ADDRESS_2),
-                Arrays.asList(task1,task2));
+        String task3 = "40,2000";
+        List<String> results = aggregator.sendTaskToWorkers(Arrays.asList(WORKER_ADDRESS_1,WORKER_ADDRESS_2,WORKER_ADDRESS_3),
+                Arrays.asList(task1,task2,task3));
 
         for (String result : results) {
             System.out.println(result);
